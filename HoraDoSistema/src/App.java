c2af61e2e776f8b970c9a2a3b1ef8c17763f034c
Luis Martins <125165369+martinssluis@users.noreply.github.com>
2024-02-06 14:25:22 -0300

    Add files via upload

---
+++ HoraDoSistema/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Date relogio = new Date();
+        System.out.print("A hora do sistema é ");
+        System.out.print(relogio.toString());
+    }
+}
